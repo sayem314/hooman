d235590d7428f3c431cb7439de27b961399c0ab7
Sayem Chowdhury <sayem314@gmail.com>
2020-05-12 22:22:03 +0600

    improve error handling

--- hooman.js
+++ hooman.js
@@ -20,8 +20,10 @@ const instance = got.extend({
   notFoundRetry: 1, // Handle redirect issue
   captchaRetry: 1,
   captchaKey: null,
-  http2: true, // Use recommended protocol
+  http2: false, // http2 doesn't work well with proxies
   headers: {
+    'accept-encoding': 'gzip, deflate',
+    'accept-language': 'en-US,en;q=0.9,bn;q=0.8',
     'cache-control': 'max-age=0',
     'upgrade-insecure-requests': '1',
     'user-agent': new UserAgent().toString(), // Use random user agent between sessions
@@ -30,6 +32,7 @@ const instance = got.extend({
     beforeRequest: [
       (options) => {
         // Add required headers to mimic browser environment
+        options.headers.host = options.url.host;
         options.headers.origin = options.url.origin;
         options.headers.referer = options.url.href;
       },
@@ -44,7 +47,7 @@ const instance = got.extend({
           response.body.includes('jschl-answer')
         ) {
           // Solve js challange
-          if (challengeInProgress[response.url.host]) {
+          if (challengeInProgress[response.url.host] && !response.request.options.ignoreInProgress) {
             while (challengeInProgress[response.url.host]) {
               // console.log(`Waiting for captcha to be solved for ${response.url.host}`);
               await delay(1000);
@@ -55,7 +58,8 @@ const instance = got.extend({
           challengeInProgress[response.url.host] = true;
           const data = await solveChallenge(response.url, response.body);
           response.request.options.cloudflareRetry--;
-          return instance({ ...response.request.options, ...data }).finally(() => {
+          return instance({ ...response.request.options, ...data, ignoreInProgress: true }).finally(() => {
+            console.log('deleted');
             delete challengeInProgress[response.url.host];
           });
         } else if (
@@ -78,7 +82,7 @@ const instance = got.extend({
           response.body.includes('cf_captcha_kind')
         ) {
           // Solve g/hCaptcha
-          if (challengeInProgress[response.url.host]) {
+          if (challengeInProgress[response.url.host] && !response.request.options.ignoreInProgress) {
             while (challengeInProgress[response.url.host]) {
               // console.log(`Waiting for captcha to be solved for ${response.url.host}`);
               await delay(1000);
@@ -92,6 +96,7 @@ const instance = got.extend({
             return instance({
               ...captchaData,
               captchaRetry: response.request.options.captchaRetry - 1,
+              ignoreInProgress: true,
             }).finally(() => {
               delete challengeInProgress[response.url.host];
             });
